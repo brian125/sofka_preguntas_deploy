fef824da91a611db27ced6d811bd8fc9c82d1c64
unknown <brayan_silva82151@elpoli.edu.co>
2021-09-10 20:15:21 -0500

    Challenge finalizado

--- config/db.js
+++ config/db.js
@@ -5,6 +5,8 @@ module.exports = new Sequelize(process.env.BD_NAME, process.env.BD_USER, process
     host: process.env.BD_HOST,
     port: process.env.BD_PORT,
     dialect: 'postgres',
+    native: true,
+    ssl: true,
     dialectOptions: {
         ssl: true
     },
